fb2747327f5611456df9be8e56708fb2b9e0e556
Shobhit <shobhitasthana0@gmail.com>
2024-05-17 17:45:23 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const expenseform = document.getElementById('expenseForm')
+const expenselist = document.getElementById('expenselist')
+expenseform.addEventListener('submit',
+    function(event){
+        event.preventDefault();
+        const description = document.getElementById('description').value;
+        const category = document.getElementById('category').value;
+        const amount = document.getElementById('amount').value;
+
+        if(document && category && !isNaN
+            (amount)){
+                const newRow = document.createElement('tr')
+
+                newRow.innerHTML = `<td>${description}</td>
+                <td>${category}</td>
+                <td>${amount}</td>`;
+
+                expenselist.appendChild(newRow)
+
+                document.getElementById('description').value=''
+                document.getElementById('category').value=''
+                document.getElementById('amount').value=''
+
+            }
+            else{
+                alert('please fill out all field with valid data')
+            }
+    }
+)
